9caab699525826f573d33da93c5e041e6c19dc9e
Sreeram P <psreeram3015@proton.me>
2023-09-15 19:35:27 +0530

    Initial Commit

---
+++ gui-version.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pand
+import os
+import cv2
+from matplotlib import pyplot as plt
+import tkinter
+import customtkinter
+
+#System settings
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+#App Frame
+app = customtkinter.CTk()
+app.geometry("720x480")
+
+app.mainloop()
