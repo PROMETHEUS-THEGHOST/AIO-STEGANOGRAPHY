ef54ea1610d82df6fe9bf6261ec03eddc4671543
Sreeram P <psreeram3015@proton.me>
2023-09-15 20:12:26 +0530

    Connected img_steg_button and img_steg. Created buttons for first part of img_steg()

--- gui-version.py
+++ gui-version.py
@@ -1,28 +1,128 @@
 import numpy as np
-import pandas as pand
+import pandas as pd  # Fixed typo in module name
 import os
 import cv2
 from matplotlib import pyplot as plt
-import tkinter
+import tkinter as tk  # Changed tkinter import
 import customtkinter
 
-#System settings
+# Functions
+def msgtobinary(msg):
+    if type(msg) == str:
+        result = ''.join([format(ord(i), "08b") for i in msg])
+
+    elif type(msg) == bytes or type(msg) == np.ndarray:
+        result = [format(i, "08b") for i in msg]
+
+    elif type(msg) == int or type(msg) == np.uint8:
+        result = format(msg, "08b")
+
+    else:
+        raise TypeError("Input type is not supported in this function")
+
+    return result
+
+def encode_img_data(img):
+    data = input("\nEnter the data to be Encoded in Image :")
+    if len(data) == 0:
+        raise ValueError('Data entered to be encoded is empty')
+
+    nameoffile = input("\nEnter the name of the New Image (Stego Image) after Encoding (with extension):")
+
+    no_of_bytes = (img.shape[0] * img.shape[1] * 3) // 8
+
+    print("\t\nMaximum bytes to encode in Image :", no_of_bytes)
+
+    if len(data) > no_of_bytes:
+        raise ValueError("Insufficient bytes Error, Need Bigger Image or give Less Data !!")
+
+    data += '*^*^*'
+
+    binary_data = msgtobinary(data)
+    print("\n")
+    print(binary_data)
+    length_data = len(binary_data)
+
+    print("\nThe Length of Binary data", length_data)
+
+    index_data = 0
+
+    for i in img:
+        for pixel in i:
+            r, g, b = msgtobinary(pixel)
+            if index_data < length_data:
+                pixel[0] = int(r[:-1] + binary_data[index_data], 2)
+                index_data += 1
+            if index_data < length_data:
+                pixel[1] = int(g[:-1] + binary_data[index_data], 2)
+                index_data += 1
+            if index_data < length_data:
+                pixel[2] = int(b[:-1] + binary_data[index_data], 2)
+                index_data += 1
+            if index_data >= length_data:
+                break
+    cv2.imwrite(nameoffile, img)
+    print("\nEncoded the data successfully in the Image, and the image is successfully saved with the name ", nameoffile)
+
+def decode_img_data(img):
+    data_binary = ""
+    for i in img:
+        for pixel in i:
+            r, g, b = msgtobinary(pixel)
+            data_binary += r[-1]
+            data_binary += g[-1]
+            data_binary += b[-1]
+            total_bytes = [data_binary[i: i + 8] for i in range(0, len(data_binary), 8)]
+            decoded_data = ""
+            for byte in total_bytes:
+                decoded_data += chr(int(byte, 2))
+                if decoded_data[-5:] == "*^*^*":
+                    print("\n\nThe Encoded data that was hidden in the Image was :--  ", decoded_data[:-5])
+                    return
+
+def img_steg():
+    while True:
+        h2_label = customtkinter.CTkLabel(app, text="IMAGE STEGANOGRAPHY OPERATIONS")
+        h2_label.pack(padx=10, pady=10)
+
+        enc_text_msg_button = customtkinter.CTkButton(app, text="Encode the Text message", width=140)
+        enc_text_msg_button.pack(padx=20, pady=10)
+        
+        dec_text_msg_button = customtkinter.CTkButton(app, text="Decode the Text message", width=140)
+        dec_text_msg_button.pack(padx=20, pady=10)
+        
+        exit_button = customtkinter.CTkButton(app, text="EXIT", width=140)
+        exit_button.pack(padx=20, pady=10)
+        
+        choice1 = int(input("Enter the Choice: "))
+        if choice1 == 1:
+            image = cv2.imread("Sample_cover_files/cover_image.jpg")
+            encode_img_data(image)
+        elif choice1 == 2:
+            image1 = cv2.imread(input("Enter the Image you need to Decode to get the Secret message :  "))
+            decode_img_data(image1)
+        elif choice1 == 3:
+            break
+        else:
+            print("Incorrect Choice")
+        print("\n")
+
+# System settings
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
 
-#App Frame
+# App Frame
 app = customtkinter.CTk()
 app.geometry("720x480")
 app.title("PROTON STEGANOGRAPHY")
 
-
-#UI Elements
+# UI Elements
 title_label = customtkinter.CTkLabel(app, text="STEGANOGRAPHY")
 title_label.pack(padx=10, pady=10)
 heading_label = customtkinter.CTkLabel(app, text="MAIN MENU")
 heading_label.pack(padx=10, pady=10)
 
-img_steg_button = customtkinter.CTkButton(app, text="IMAGE STEGANOGRAPHY", width=140)
+img_steg_button = customtkinter.CTkButton(app, text="IMAGE STEGANOGRAPHY", width=140, command=img_steg)
 img_steg_button.pack(padx=20, pady=10)
 
 txt_steg_button = customtkinter.CTkButton(app, text="TEXT STEGANOGRAPHY", width=140)
