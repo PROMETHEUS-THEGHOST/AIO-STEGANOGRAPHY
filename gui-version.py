02cbf1e92b4e7da8c8f70cd88be4d4e0bdfedec3
Sreeram P <psreeram3015@proton.me>
2023-09-15 19:49:21 +0530

    Written the title and created buttons in the gui

--- gui-version.py
+++ gui-version.py
@@ -13,5 +13,28 @@ customtkinter.set_default_color_theme("blue")
 #App Frame
 app = customtkinter.CTk()
 app.geometry("720x480")
+app.title("PROTON STEGANOGRAPHY")
+
+
+#UI Elements
+title_label = customtkinter.CTkLabel(app, text="STEGANOGRAPHY")
+title_label.pack(padx=10, pady=10)
+heading_label = customtkinter.CTkLabel(app, text="MAIN MENU")
+heading_label.pack(padx=10, pady=10)
+
+img_steg_button = customtkinter.CTkButton(app, text="IMAGE STEGANOGRAPHY", width=140)
+img_steg_button.pack(padx=20, pady=10)
+
+txt_steg_button = customtkinter.CTkButton(app, text="TEXT STEGANOGRAPHY", width=140)
+txt_steg_button.pack(padx=20, pady=10)
+
+aud_steg_button = customtkinter.CTkButton(app, text="AUDIO STEGANOGRAPHY", width=140)
+aud_steg_button.pack(padx=20, pady=10)
+
+vid_steg_button = customtkinter.CTkButton(app, text="VIDEO STEGANOGRAPHY", width=140)
+vid_steg_button.pack(padx=20, pady=10)
+
+exit_button = customtkinter.CTkButton(app, text="EXIT", width=140)
+exit_button.pack(padx=20, pady=10)
 
 app.mainloop()
